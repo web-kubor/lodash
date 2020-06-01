31147ef48a6a4ca92e37017206ae853035ccd996
webkubor <webkubor@163.com>
2020-06-01 15:33:08 +0800

    对象克隆

--- .internal/copyObject.js
+++ .internal/copyObject.js
@@ -2,15 +2,15 @@ import assignValue from './assignValue.js'
 import baseAssignValue from './baseAssignValue.js'
 
 /**
- * Copies properties of `source` to `object`.
+ *将“源”的属性复制到“对象”。
  *
- * @private
- * @param {Object} source The object to copy properties from.
- * @param {Array} props The property identifiers to copy.
- * @param {Object} [object={}] The object to copy properties to.
- * @param {Function} [customizer] The function to customize copied values.
- * @returns {Object} Returns `object`.
- */
+ *@私人的
+ *@param {Object}源从中复制属性的对象。
+ *@param {Array} props要复制的属性标识符。
+ *@param {Object} [object = {}]复制属性的对象。
+ *@param {Function} [customizer]用于自定义复制值的函数。
+ *@returns {Object}返回`object`。
+ */ 
 function copyObject(source, props, object, customizer) {
   const isNew = !object
   object || (object = {})
